77cf0c42a70051597988fa98f0f150695f2d9fd0
shiva508 <dasarishiva1@gmail.com>
2024-05-26 16:59:14 +0530

    basic entrypoint authentication

--- src/main/java/com/comrade/config/DhaSecurityConfiguration.java
+++ src/main/java/com/comrade/config/DhaSecurityConfiguration.java
@@ -15,7 +15,6 @@ import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
 public class DhaSecurityConfiguration {
